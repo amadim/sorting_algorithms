a41e9e22c3996804a96211c7612fefab31e59aba
amadim <amadimariana@gmail.com>
2022-11-10 14:25:15 -0800

    107-quick_sort_hoare.c

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -89,7 +89,7 @@ void hoare_sort(int *array, size_t size, int left, int right)
 void quick_sort_hoare(int *array, size_t size)
 {
 	if (array == NULL || size < 2)
-return;
+		return;
 
 	hoare_sort(array, size, 0, size - 1);
 }
